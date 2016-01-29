e2cdc84d3769c01ce9e489bbdcd970df14dc19eb
Jose Soto <jose@homehero.org>
2016-01-28 23:11:34 -0800

    docker alias

--- oh-my-zsh-custom/custom/aliases.zsh
+++ oh-my-zsh-custom/custom/aliases.zsh
@@ -17,4 +17,4 @@ alias vu="vagrant up"
 alias vh="vagrant halt"
 alias vd="vagrant destroy"
 alias composer="hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 /usr/local/bin/composer"
-
+alias dockerenv="eval $(docker-machine env default)"
