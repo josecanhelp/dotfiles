30e2afd5c03323de6ed2e893b9531d8ba1a39760
Jose Soto <josecanhelp@gmail.com>
2017-07-12 16:44:28 -0700

    Adds some comments

--- oh-my-zsh-custom/custom/functions.zsh
+++ oh-my-zsh-custom/custom/functions.zsh
@@ -1,22 +1,23 @@
 # Create a new directory and enter it
-  function mkd() {
-    mkdir -p "$@" && cd "$_";
-  }
+function mkd() {
+  mkdir -p "$@" && cd "$_";
+}
 
- function gpp() {
-    git push origin HEAD
+# Open the pull request url for your current directory's branch (base branch defaults to master)
+function openpr() {
+  github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%'`;
+  branch_name=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`;
+  pr_url=$github_url"/compare/master..."$branch_name
+  open $pr_url;
+}
 
-    if [ $? -eq 0 ]; then
-    	openpr
-    else
-    	echo 'failed to push commits and open a pull request.';
-    fi
+# Run git push and then immediately open the pull request url
+function gpp() {
+  git push origin HEAD
 
- }
-
- function openpr() {
-    github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%'`;
-    branch_name=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`;
-    pr_url=$github_url"/compare/master..."$branch_name
-    open $pr_url;
- }
+  if [ $? -eq 0 ]; then
+    openpr
+  else
+    echo 'failed to push commits and open a pull request.';
+  fi
+}
