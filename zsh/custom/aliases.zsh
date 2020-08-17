9ffe6010d939051fd39c4c8c0dc14763ca664993
Jose Soto <josecanhelp@gmail.com>
2020-08-17 10:42:58 -0700

    vi-mode improvements

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -47,3 +47,7 @@ alias resetwacom="launchctl unload /Library/LaunchAgents/com.wacom.wacomtablet.p
 alias l="ls -alh"
 alias src="source ~/.zshrc"
 alias ..="cd .."
+alias zz='z -c'      # restrict matches to subdirs of $PWD
+alias zi='z -i'      # cd with interactive selection
+alias zf='z -I'      # use fzf to select in multiple matches
+alias zb='z -b'      # quickly cd to the parent directory
