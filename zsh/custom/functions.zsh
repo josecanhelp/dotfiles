7dbad291afd51fbfaedf297a966e4be71f3a607d
Jose Soto <josecanhelp@gmail.com>
2020-08-17 15:19:22 -0700

    Updates

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -60,3 +60,14 @@ function toggleXdebug() {
         sed -i '/^;zend.*xdebug.so\"$ /s/^;//' $(getPhpIni)
   fi
 }
+
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+function current_repository() {
+
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo $(git remote -v | cut -d':' -f 2)
+}
