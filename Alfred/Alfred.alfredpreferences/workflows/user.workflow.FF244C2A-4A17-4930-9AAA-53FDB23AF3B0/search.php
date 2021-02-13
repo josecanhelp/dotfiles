acb856c7160783153e5f462b2dd539cff6dbe24c
Jose Soto <josecanhelp@gmail.com>
2021-02-12 19:05:39 -0800

    Ram Workflow Improvements

--- Alfred/Alfred.alfredpreferences/workflows/user.workflow.FF244C2A-4A17-4930-9AAA-53FDB23AF3B0/search.php
+++ Alfred/Alfred.alfredpreferences/workflows/user.workflow.FF244C2A-4A17-4930-9AAA-53FDB23AF3B0/search.php
@@ -9,8 +9,8 @@ $searchQuery = $argv[1];
 $token = getenv('token');
 
 if (! $token) {
-  echo json_encode(['items' => [new Item('No Master Key set', 'Use the ramkey to set the master key', null)]]);
-  return;
+    echo json_encode(['items' => [new Item('No Master Key set', 'Use the ramkey to set the master key', null)]]);
+    return;
 }
 
 $client = new Client('http://ram.tighten.co:7700', $token);
@@ -20,12 +20,16 @@ $result = $index->search($searchQuery);
 
 $alfredItems = [];
 foreach ($result['hits'] as $entry) {
-  $alfredItems[] = new Item($entry['content'], $entry['author'], $entry['id']);
+    $alfredItems[] = new Item(
+        $entry['content'],
+        $entry['author'],
+        implode('±', ['content' => $entry['content'], 'id' => $entry['id']]),
+    );
 }
 
 if (empty($alfredItems)) {
-  echo json_encode(['items' => [new Item('No matching results found', 'Try a different seach query', null)]]);
-  return;
+    echo json_encode(['items' => [new Item('No matching results found', 'Try a different seach query', null)]]);
+    return;
 }
 
 print_r(json_encode(['items' => $alfredItems]));
