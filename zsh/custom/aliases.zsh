1c55e1606df8ff2c9ac96612b40cebfe52407a48
Jose Soto <josecanhelp@gmail.com>
2022-06-30 08:26:29 -0700

    Remove old git alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,5 +1,4 @@
 alias heroky="heroku"
-alias git="git"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp="git push origin HEAD"
 alias gd="git diff"
