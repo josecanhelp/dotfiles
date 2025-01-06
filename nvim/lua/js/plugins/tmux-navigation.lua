766f2e773b7a2400a0b19b55c83deaf72a0d1406
Jose Soto <josecanhelp@gmail.com>
2025-01-06 07:57:45 -0800

    WIP

--- nvim/lua/js/plugins/tmux-navigation.lua
+++ nvim/lua/js/plugins/tmux-navigation.lua
@@ -1,6 +1,5 @@
 return {
   "christoomey/vim-tmux-navigator",
-  event = "VeryLazy", -- Optional: load plugin only when needed
   keys = {
     { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left with tmux" },
     { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down with tmux" },
