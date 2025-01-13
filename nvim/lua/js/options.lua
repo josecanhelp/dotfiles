e81851424e9c5c5f08e7508b8db1368c96d823a3
Jose Soto <josecanhelp@gmail.com>
2025-01-13 07:29:16 -0800

    Re-add nvim options

--- nvim/lua/js/options.lua
+++ nvim/lua/js/options.lua
@@ -1,36 +1,35 @@
 --------------------------------------------------------------------------------
 -- General Vim Options
 --------------------------------------------------------------------------------
-
--- vim.opt.tabstop = 4
--- vim.opt.softtabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true
--- vim.opt.hidden = true
--- vim.opt.backup = false
--- vim.opt.swapfile = false
--- vim.opt.autoread = true
--- vim.opt.confirm = true
--- vim.opt.encoding = 'utf-8'
--- vim.opt.clipboard = 'unnamed'
--- vim.opt.backspace = { 'indent', 'eol', 'start' }
--- vim.opt.number = true
--- vim.opt.relativenumber = true
--- vim.opt.cursorline = true
--- vim.opt.cursorlineopt = 'number'
--- vim.opt.showmode = false
--- vim.opt.splitbelow = true
--- vim.opt.splitright = true
--- vim.opt.fillchars = { vert = ' ' }
--- vim.opt.hlsearch = true
--- vim.opt.ignorecase = true
--- vim.opt.smartcase = true
--- vim.opt.mouse = 'a'
--- vim.opt.scrolloff = 5
--- vim.opt.updatetime = 1000
--- vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'noselect' }
--- vim.opt.wrap = false
--- vim.opt.autoindent = true
--- vim.opt.smartindent = true
--- vim.opt.exrc = true
--- vim.opt.secure = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.autoread = true
+vim.opt.confirm = true
+vim.opt.encoding = 'utf-8'
+vim.opt.clipboard = 'unnamed'
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
+vim.opt.showmode = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.fillchars = { vert = ' ' }
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'
+vim.opt.scrolloff = 5
+vim.opt.updatetime = 1000
+vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'noselect' }
+vim.opt.wrap = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.exrc = true
+vim.opt.secure = true
